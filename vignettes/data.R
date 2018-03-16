4b58c63ad38f08a61cf26ecce2b357220ea2d54d
taha@fushan <taha@chepec.se>
2018-03-16 04:39:44 +0100

    Vignette includes all data as LaTeX table.

--- vignettes/data.R
+++ vignettes/data.R
@@ -26,9 +26,9 @@ options(tikzDefaultEngine = "pdftex",
    stringsAsFactors = FALSE)
 
 ## ----'load-R-packages', echo=F, results='hide', message=FALSE---------------------
-# packages here should also be listed in DESCRIPTION
+library(bandgaps)
+# packages listed below should also be listed in DESCRIPTION
 library(common)
-# library(bandgaps)
 library(git2r)
    repo <- repository("/media/bay/taha/chepec/chetex/common/R/bandgaps")
 library(ggplot2)
@@ -41,7 +41,7 @@ opts_knit$set(self.contained = FALSE,
 opts_chunk$set(dev = 'tikz',
                # external = TRUE only takes effect when tikz is the ONLY dev option
                external = TRUE,
-               cache = TRUE,
+               cache = FALSE,
                fig.width = 3.40, # 2.60,
                # corresponding height based on golden ratio (1.618034):
                # W 3.54 ==> H 2.19 >> works better >> W 2.60, H 1.61
@@ -58,6 +58,254 @@ opts_chunk$set(dev = 'tikz',
                results = 'hide',
                message = FALSE,
                tidy = FALSE)
+
+## ----'comments-to-footnotes'------------------------------------------------------
+comments <- unique(semiconductors$comment)
+# Count existing comments and remove empty comments
+k <- 0
+tmp.comments <- character()
+for (j in 1:length(comments)) {
+   if (comments[j] != "") {
+      k <- k + 1
+      tmp.comments[k] <- comments[j]
+   }
+}
+# Reset comments
+comments <- tmp.comments
+# Find their original positions in the semiconductors dataframe
+unique.comment.positions <- list()
+for (j in 1:length(comments)) {
+   unique.comment.positions[[j]] <- which(semiconductors$comment == comments[j])
+}
+## create-footnotemarks
+footnotemark <- rep.int(NA, dim(semiconductors)[1])
+for (j in 1:length(unique.comment.positions)) {
+   footnotemark[unique.comment.positions[[j]]] <- j
+}
+
+## ----'tab-semiconductors-asis', results='asis'------------------------------------
+xtab.semiconductors <-
+   semiconductors %>%
+   mutate(formula =
+             ifelse(semiconductors$polymorph == "",
+                    paste0(ifelse(semiconductors$sctype == "",
+                                  "",
+                                  paste0("$", semiconductors$sctype, "$-")),
+                           "\\ch{", semiconductors$formula, "}"),
+                    paste0(ifelse(semiconductors$sctype == "",
+                                  "",
+                                  paste0("$", semiconductors$sctype, "$-")),
+                           "\\ch{", semiconductors$formula, "} ", "(", semiconductors$polymorph, ")"))) %>%
+   mutate(pH =
+             ifelse(is.na(semiconductors$pH),
+                    "\\multicolumn{1}{c}{\\texttt{NA}}",
+                    semiconductors$pH)) %>%
+   mutate(pH.ZPC =
+             ifelse(is.na(semiconductors$pH.ZPC),
+                    "\\multicolumn{1}{c}{\\texttt{NA}}",
+                    semiconductors$pH.ZPC)) %>%
+   mutate(ref =
+             ifelse(semiconductors$ref == "",
+                    "",
+                    paste0("\\cite{", semiconductors$ref, "}"))) %>%
+   mutate(footnotemark =
+             ifelse(is.na(footnotemark),
+                    "",
+                    paste0("$^{", footnotemark, "}$"))) %>%
+   select(formula, class, CB, VB, Eg, pH, pH.ZPC, ref, footnotemark) %>%
+   xtable()
+caption(xtab.semiconductors) <-
+   paste("Band edge levels at the pH of ZPC for each material.",
+         "All band edge potentials vs SHE.")
+label(xtab.semiconductors) <- "tab:semiconductors-asis"
+names(xtab.semiconductors) <-
+ c("{Formula}",
+   "{Class}",
+   "{$E_\\text{CB}$/\\si{\\voltSHE}}",
+   "{$E_\\text{VB}$/\\si{\\voltSHE}}",
+   "{$E_\\text{g}$/\\si{\\volt}}",
+   "{pH}",
+   "{pH$_\\text{ZPC}$}",
+   "{Ref}",
+   "{Note}")
+digits(xtab.semiconductors) <-
+ c(0, #row.names
+   0, #formula
+   0, #class
+   2, #CB
+   2, #VB
+   2, #Eg
+   2, #pH
+   2, #pH.ZPC
+   0, #Refs
+   0) #Notes
+display(xtab.semiconductors) <-
+ c("s", #row.names
+   "s", #formula
+   "s", #class
+   "f", #CB
+   "f", #VB
+   "f", #Eg
+   "f", #pH
+   "f", #pH.ZPC
+   "s", #Refs
+   "s") #Notes
+align(xtab.semiconductors) <-
+ c("l",                    #row.names
+   "l",                    #formula
+   "l",                    #class
+   "S[table-format=+1.2]", #CB
+   "S[table-format=+1.2]", #VB
+   "S[table-format=+1.2]", #Eg
+   "S[table-format=1.2]",  #pH
+   "S[table-format=1.2]",  #pH.ZPC
+   "c",                    #Refs
+   "l")                    #Notes
+## create-latex-footnotetexts
+footnotetext <- comments
+# add numbering to each footnote
+for (j in 1:length(comments)) {
+   footnotetext[j] <-
+      paste0("\\multicolumn{", dim(xtab.semiconductors)[2],
+             "}{l}{", "$^{", j, "}$ ", comments[j], "}",
+             ifelse(j == length(comments),
+                    "\n",
+                    "\\\\"))
+}
+print(xtab.semiconductors,
+      floating = TRUE,
+      floating.environment = "table",
+      table.placement = "tbp",
+      caption.placement = "top",
+      hline.after = NULL,
+      add.to.row = list(pos = list(-1,
+                                   0,
+                                   nrow(xtab.semiconductors),
+                                   nrow(xtab.semiconductors)),
+                        command = c("\\toprule\n",
+                                    "\\midrule\n",
+                                    "\\bottomrule\n",
+                                    paste(footnotetext, collapse = "\n"))),
+      include.rownames = FALSE,
+      include.colnames = TRUE,
+      type = "latex",
+      tabular.environment = "tabular",
+      latex.environments = c("center", "small"),
+      # note: env small affects the table and footnotetext, but not the caption
+      sanitize.text.function = function(x){x},
+      math.style.negative = FALSE)
+
+## ----'print-tab-semiconductors-asis', ref.label='tab-semiconductors-asis', eval=FALSE, echo=TRUE----
+#  xtab.semiconductors <-
+#     semiconductors %>%
+#     mutate(formula =
+#               ifelse(semiconductors$polymorph == "",
+#                      paste0(ifelse(semiconductors$sctype == "",
+#                                    "",
+#                                    paste0("$", semiconductors$sctype, "$-")),
+#                             "\\ch{", semiconductors$formula, "}"),
+#                      paste0(ifelse(semiconductors$sctype == "",
+#                                    "",
+#                                    paste0("$", semiconductors$sctype, "$-")),
+#                             "\\ch{", semiconductors$formula, "} ", "(", semiconductors$polymorph, ")"))) %>%
+#     mutate(pH =
+#               ifelse(is.na(semiconductors$pH),
+#                      "\\multicolumn{1}{c}{\\texttt{NA}}",
+#                      semiconductors$pH)) %>%
+#     mutate(pH.ZPC =
+#               ifelse(is.na(semiconductors$pH.ZPC),
+#                      "\\multicolumn{1}{c}{\\texttt{NA}}",
+#                      semiconductors$pH.ZPC)) %>%
+#     mutate(ref =
+#               ifelse(semiconductors$ref == "",
+#                      "",
+#                      paste0("\\cite{", semiconductors$ref, "}"))) %>%
+#     mutate(footnotemark =
+#               ifelse(is.na(footnotemark),
+#                      "",
+#                      paste0("$^{", footnotemark, "}$"))) %>%
+#     select(formula, class, CB, VB, Eg, pH, pH.ZPC, ref, footnotemark) %>%
+#     xtable()
+#  caption(xtab.semiconductors) <-
+#     paste("Band edge levels at the pH of ZPC for each material.",
+#           "All band edge potentials vs SHE.")
+#  label(xtab.semiconductors) <- "tab:semiconductors-asis"
+#  names(xtab.semiconductors) <-
+#   c("{Formula}",
+#     "{Class}",
+#     "{$E_\\text{CB}$/\\si{\\voltSHE}}",
+#     "{$E_\\text{VB}$/\\si{\\voltSHE}}",
+#     "{$E_\\text{g}$/\\si{\\volt}}",
+#     "{pH}",
+#     "{pH$_\\text{ZPC}$}",
+#     "{Ref}",
+#     "{Note}")
+#  digits(xtab.semiconductors) <-
+#   c(0, #row.names
+#     0, #formula
+#     0, #class
+#     2, #CB
+#     2, #VB
+#     2, #Eg
+#     2, #pH
+#     2, #pH.ZPC
+#     0, #Refs
+#     0) #Notes
+#  display(xtab.semiconductors) <-
+#   c("s", #row.names
+#     "s", #formula
+#     "s", #class
+#     "f", #CB
+#     "f", #VB
+#     "f", #Eg
+#     "f", #pH
+#     "f", #pH.ZPC
+#     "s", #Refs
+#     "s") #Notes
+#  align(xtab.semiconductors) <-
+#   c("l",                    #row.names
+#     "l",                    #formula
+#     "l",                    #class
+#     "S[table-format=+1.2]", #CB
+#     "S[table-format=+1.2]", #VB
+#     "S[table-format=+1.2]", #Eg
+#     "S[table-format=1.2]",  #pH
+#     "S[table-format=1.2]",  #pH.ZPC
+#     "c",                    #Refs
+#     "l")                    #Notes
+#  ## create-latex-footnotetexts
+#  footnotetext <- comments
+#  # add numbering to each footnote
+#  for (j in 1:length(comments)) {
+#     footnotetext[j] <-
+#        paste0("\\multicolumn{", dim(xtab.semiconductors)[2],
+#               "}{l}{", "$^{", j, "}$ ", comments[j], "}",
+#               ifelse(j == length(comments),
+#                      "\n",
+#                      "\\\\"))
+#  }
+#  print(xtab.semiconductors,
+#        floating = TRUE,
+#        floating.environment = "table",
+#        table.placement = "tbp",
+#        caption.placement = "top",
+#        hline.after = NULL,
+#        add.to.row = list(pos = list(-1,
+#                                     0,
+#                                     nrow(xtab.semiconductors),
+#                                     nrow(xtab.semiconductors)),
+#                          command = c("\\toprule\n",
+#                                      "\\midrule\n",
+#                                      "\\bottomrule\n",
+#                                      paste(footnotetext, collapse = "\n"))),
+#        include.rownames = FALSE,
+#        include.colnames = TRUE,
+#        type = "latex",
+#        tabular.environment = "tabular",
+#        latex.environments = c("center", "small"),
+#        # note: env small affects the table and footnotetext, but not the caption
+#        sanitize.text.function = function(x){x},
+#        math.style.negative = FALSE)
 
 ## ----'gitR-info', echo=FALSE, results='markup', cache=FALSE-----------------------
 status(repo)
